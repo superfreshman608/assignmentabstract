f22d7f67a8099dbf53d0931ca675f738550197e6
superfreshman <burrujer000@warren.k12.in.us>
2025-03-06 09:44:52 -0500

    initial commit

---
+++ abstract assignment.py
@@ -0,0 +1,28 @@
+#Assignment 1
+from abc import ABC, abstractmethod
+from math import pi
+
+class Shape(ABC)
+
+    def area(self):
+
+class Circle(shape)
+    def __init__(self, radius):
+
+#Assignment 2
+
+class BankAccount
+def __init__(self, balance, owner):
+self.balance = balance
+self.owner = owner
+
+def deposit(self, amount):
+self.balance += amount
+
+def withdraw(self, amount):
+self.balance -= amount
+
+
+
+
+
